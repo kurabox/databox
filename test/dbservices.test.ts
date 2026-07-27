6c62f974923d248b3cf34ff500273b9b9a573765
thinh-tr <ducthinh2049@gmail.com>
2026-07-27 16:57:57 +0700

    test: cập nhật test-case

--- test/dbservices.test.ts
+++ test/dbservices.test.ts
@@ -1,6 +1,7 @@
 import { assert, assertEquals } from "@std/assert";
-import { DbConfig, loadEnvDbConfig, initSequelizeInstance } from "../src/dbservices.ts";
-import { Sequelize } from "sequelize";
+import { DbConfig, loadEnvDbConfig, initSequelizeInstance, modelingIndexDbEntities } from "../src/dbservices.ts";
+import { Sequelize, Transaction } from "sequelize";
+import { Page } from "../src/entities.ts";
 
 Deno.test("load env config test", (): void => {
     const config: DbConfig | null = loadEnvDbConfig();
@@ -15,4 +16,18 @@ Deno.test("load env config test", (): void => {
 Deno.test("init sequelize instance test", async (): Promise<void> => {
     const sequelize: Sequelize = initSequelizeInstance(loadEnvDbConfig(), "mariadb");
     await sequelize.authenticate();
+});
+
+Deno.test("sample query test", async (): Promise<void> => {
+    const dbConfig: DbConfig = loadEnvDbConfig();
+    const sequelize: Sequelize = initSequelizeInstance(dbConfig, "mariadb");
+    modelingIndexDbEntities(sequelize);
+    const t: Transaction = await sequelize.transaction();
+    try {
+        await Page.create({ id: "id01", url: "www.example.com" }, { transaction: t });
+        await t.commit();   // commit các thay đổi tư transaction vào database
+    } catch (err: unknown) {
+        await t.rollback();
+        console.log(err);
+    }
 });
